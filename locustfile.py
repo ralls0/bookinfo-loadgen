50d0ba8bc2a8679fce462e01516c601b4da7c70b
Marco Manco <marcomanco1998@gmail.com>
2022-09-01 17:25:20 +0200

    feat: add loadgen

---
+++ locustfile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import random
+from locust import HttpUser, TaskSet, between
+
+
+def index(l):
+    l.client.get("/productpage")
+
+
+class UserBehavior(TaskSet):
+
+    def on_start(self):
+        index(self)
+
+    tasks = {index: 1}
+
+
+class WebsiteUser(HttpUser):
+    tasks = [UserBehavior]
+    wait_time = between(1, 10)
